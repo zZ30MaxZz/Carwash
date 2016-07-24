f8216885e5f13a281f00c05da8d3a7e84427bcfc
Max Aguilar <zz30.maxzz@gmail.com>
2016-07-24 10:47:16 -0500

    Habilitamos el generador
    Para poder programar más rápido

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<title>ffff<?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
